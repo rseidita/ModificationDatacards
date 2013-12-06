a3164a136e179ed9d5f101b51f413c0649fb0590
Andrea <massironi.andrea@gmail.com>
2013-12-06 12:42:52 +0100

    first import

---
+++ inputScaleNuisance.py
@@ -0,0 +1,8 @@
+#
+# scale nuisance: scale VV by a factor YY
+#
+
+systScaleFactor.update({'CMS_8TeV_hww_WJet_FakeRate_e_shape_2j'  : '1.5'})
+systScaleFactor.update({'CMS_8TeV_hww_WJet_FakeRate_m_shape_2j'  : '2.5'})
+
+systScaleFactor.update({'lumi_8TeV'                              : '2.5'})
