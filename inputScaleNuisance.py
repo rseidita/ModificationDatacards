5627f9d1d5238ec09e4835ddc77c2afa6f479dad
Andrea <massironi.andrea@gmail.com>
2013-12-10 14:29:12 +0100

    up

--- inputScaleNuisance.py
+++ inputScaleNuisance.py
@@ -2,9 +2,30 @@
 # scale nuisance: scale VV by a factor YY
 #
 
-systScaleFactor.update({'CMS_8TeV_hww_WJet_FakeRate_e_shape_2j'  : '1.5'})
-systScaleFactor.update({'CMS_8TeV_hww_WJet_FakeRate_m_shape_2j'  : '2.5'})
+#systScaleFactor.update({'CMS_hww_MVAWEBounding'  : '1.4'})  # sqrt(2) ~ 1.4
+#systScaleFactor.update({'CMS_hww_MVAWMBounding'  : '1.4'})  # sqrt(2) ~ 1.4
 
-systScaleFactor.update({'CMS_8TeV_p_scale_e'  : '2.5'})
+#systScaleFactor.update({'CMS_hwwof_0j_MVAWjetsEStatBounding_8TeV'  : '1.4'})  # sqrt(2) ~ 1.4
+#systScaleFactor.update({'CMS_hwwof_0j_MVAWjetsMStatBounding_8TeV'  : '1.4'})  # sqrt(2) ~ 1.4
 
-systScaleFactor.update({'lumi_8TeV'                              : '2.5'})
+#systScaleFactor.update({'CMS_hwwof_1j_MVAWjetsEStatBounding_8TeV'  : '1.4'})  # sqrt(2) ~ 1.4
+#systScaleFactor.update({'CMS_hwwof_1j_MVAWjetsMStatBounding_8TeV'  : '1.4'})  # sqrt(2) ~ 1.4
+
+#systScaleFactor.update({'CMS_hwwof_2j_MVAWjetsEStatBounding_8TeV'  : '1.4'})  # sqrt(2) ~ 1.4
+#systScaleFactor.update({'CMS_hwwof_2j_MVAWjetsMStatBounding_8TeV'  : '1.4'})  # sqrt(2) ~ 1.4
+
+systScaleFactor.update({'CMS_hwwof_0j_MVAWjetsEStatBounding_8TeV'  : '3.2'})  # sqrt(10) ~ 3.2
+systScaleFactor.update({'CMS_hwwof_0j_MVAWjetsMStatBounding_8TeV'  : '3.2'})  # sqrt(10) ~ 3.2
+
+systScaleFactor.update({'CMS_hwwof_1j_MVAWjetsEStatBounding_8TeV'  : '3.2'})  # sqrt(10) ~ 3.2
+systScaleFactor.update({'CMS_hwwof_1j_MVAWjetsMStatBounding_8TeV'  : '3.2'})  # sqrt(10) ~ 3.2
+
+systScaleFactor.update({'CMS_hwwof_2j_MVAWjetsEStatBounding_8TeV'  : '3.2'})  # sqrt(10) ~ 3.2
+systScaleFactor.update({'CMS_hwwof_2j_MVAWjetsMStatBounding_8TeV'  : '3.2'})  # sqrt(10) ~ 3.2
+
+systScaleFactor.update({'CMS_hwwsf_stat_1j_WjetsE_8TeV'  : '3.2'})  # sqrt(10) ~ 3.2
+systScaleFactor.update({'CMS_hwwsf_stat_1j_WjetsM_8TeV'  : '3.2'})  # sqrt(10) ~ 3.2
+
+systScaleFactor.update({'CMS_hwwsf_stat_0j_WjetsE_8TeV'  : '3.2'})  # sqrt(10) ~ 3.2
+systScaleFactor.update({'CMS_hwwsf_stat_0j_WjetsM_8TeV'  : '3.2'})  # sqrt(10) ~ 3.2
+
