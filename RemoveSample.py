b9b2d313e1f05a28dd67d87b83356905ab89d91d
Andrea <massironi.andrea@gmail.com>
2014-01-18 18:52:23 +0100

    ok copy

--- RemoveSample.py
+++ RemoveSample.py
@@ -60,46 +60,87 @@ for line in lines:
 
 print header
 
+print "#####################################"
+#print DC.list_of_bins
+listOfBins = DC.list_of_bins()
+for binIterator in listOfBins:
+    print binIterator
+print "#####################################"
 
 
+# write new datacard
+filename = 'test.txt'
+f = open(filename, 'w')
+
+# header
+for line in header: f.write (line + '\n')
+f.write ("---------------------------------------------------------------------------------------------------- \n")
+# bin name
+f.write ("bin                                 ")
+for channels in DC.exp:
+    for samples in DC.exp[channels]:
+        f.write ("%13s " % channels)
+f.write("\n")
+
+# process names (a.k.a. samples)
+f.write ("process                            ")
+for channels in DC.exp:
+    for samples in DC.exp[channels]:
+        f.write ("%13s " % samples)
+f.write("\n")
+
+# indices numbers: -1  0  1  2  3 ...
+f.write ("process                            ")
+for channels in DC.exp:
+    numSig = 0
+    numBkg = 1
+    for samples in DC.exp[channels]:
+        if DC.isSignal[samples] :
+            f.write ("%13f " % numSig)
+            numSig = numSig - 1
+        else :
+            f.write ("%13f " % numBkg)
+            numBkg = numBkg + 1
+f.write("\n")
+
+# rate
+f.write ("rate ")
+for channel in DC.exp:
+    for process in DC.exp[channel]:
+        f.write ("%9.4f " % DC.exp[channel][process] )
+f.write("\n")
+
+# systematics
+## list of [(name of uncert, boolean to indicate whether to float this nuisance or not, type, list of what additional arguments (e.g. for gmN), keyline element)]
+for nuis in nuisToConsider:
+    f.write ("%25s" % nuis[0]) # name
+    if nuis[2] != 'gmN':
+        f.write ("%10s" % nuis[2]) #lnN
+    else :
+        f.write ("%5s" % nuis[2]) # gmN
+        f.write ("%5s" % nuis[3][0]) # Ncontrol
+
+    f.write (" ")
+
+    for channel in DC.exp:
+        for process in DC.exp[channels]:
+            if channel in nuis[4]:
+                if process in nuis[4][channel] :
+                    if not isinstance ( nuis[4][channel][process], float ) :
+                    # [0.95, 1.23]  ---> from 0.95/1.23
+                        f.write ("   {0:4.3f}/{1:4.3f}".format(nuis[4][channel][process][0], nuis[4][channel][process][1]))
+                    else :
+                        if (nuis[4][channel][process] != 0) :
+                            f.write ("%9.4f" % nuis[4][channel][process])
+                        else :
+                            f.write ("%13s" % "-")
+                else :
+                    f.write ("%13s" % "-")
+            else :
+                f.write ("%13s" % "-")
+    f.write("\n")
+f.close ()
 
 
-
-# DUMP DC INTO A TXT FILE!!!
-# USE OLD CODE TO COPY THE HEADER!
-
-
-#for nuis in nuisToConsider:
-    #if nuis[2] == 'gmN': gmN = nuis[3][0]
-    #else               : gmN = 0
-    #for channel in nuis[4]:
-        ##print channel
-        #if channel not in errors.keys(): errors[channel] = {}
-        #for process in nuis[4][channel]:
-            #if nuis[2] == 'gmN': gmN = nuis[3][0]
-            #if nuis[4][channel][process] == 0: continue
-            ##print nuis[2],gmN
-            #if gmN != 0:
-                #newError = nuis[4][channel][process] * sqrt(gmN) / DC.exp[channel][process]
-            #else:
-                ##print nuis[4][channel][process]
-                #if not isinstance ( nuis[4][channel][process], float ) :
-                    ## [0.95, 1.23]
-                ##if len(nuis[4][channel][process]) == 2 :
-                    #newError = fabs((nuis[4][channel][process][1]-nuis[4][channel][process][0])/2.)   # symmetrized
-                #else : 
-                    #newError = fabs(1-nuis[4][channel][process])
-            #if process in errors[channel].keys():
-                #errors[channel][process] += newError*newError
-            #else:
-                #errors[channel][process] = newError*newError
-
-#for channel in errors:
-    #for process in errors[channel]:
-        #errors[channel][process] = sqrt(errors[channel][process])
-
-#for x in DC.exp:
-    #for y in DC.exp[x]:
-        #print "%10s %10s %10.2f +/- %10.2f (rel = %10.2f)" % (x,y,DC.exp[x][y],DC.exp[x][y]*errors[x][y],errors[x][y])
 
 
