052a3a64d20f4a94aab51e47f8bca2b0ada81341
Andrea <massironi.andrea@gmail.com>
2015-01-22 19:35:43 +0100

    update

--- examples/inputScale7TeV_125_125.6.py
+++ examples/inputScale7TeV_125_125.6.py
@@ -2,8 +2,8 @@
 # scale from 125.0 to 125.6 GeV Higgs mass
 #
 
-scaleFactor.update({'ggH'     : 15.176000/15.320000*0.224600/0.215000 })
-scaleFactor.update({'qqH'     : 1.217400/1.222000*0.224600/0.215000 }) 
+scaleFactor.update({'ggH'     : 15.176000/15.320000*0.224600/0.215000 }) ## = 1.034832e+00 ~ 1.035
+scaleFactor.update({'qqH'     : 1.217400/1.222000*0.224600/0.215000 })   ## = 1.040719e+00 ~ 1.041
 scaleFactor.update({'ggH_sbi' : 15.176000/15.320000*0.224600/0.215000 })
 scaleFactor.update({'ggH_b'   : 15.176000/15.320000*0.224600/0.215000 })
 scaleFactor.update({'ggH_s'   : 15.176000/15.320000*0.224600/0.215000 })
--- examples/inputScale8TeV_125_125.6.py
+++ examples/inputScale8TeV_125_125.6.py
@@ -2,8 +2,8 @@
 # scale from 125.0 to 125.6 GeV Higgs mass
 #
 
-scaleFactor.update({'ggH'     : 19.340000/19.520000*0.224600/0.215000 })
-scaleFactor.update({'qqH'     : 1.572000/1.578000*0.224600/0.215000 }) 
+scaleFactor.update({'ggH'     : 19.340000/19.520000*0.224600/0.215000 }) ## = 1.035018e+00 ~ 1.035 
+scaleFactor.update({'qqH'     : 1.572000/1.578000*0.224600/0.215000 })   ## = 1.040679e+00 ~ 1.041
 scaleFactor.update({'ggH_sbi' : 19.340000/19.520000*0.224600/0.215000 })
 scaleFactor.update({'ggH_b'   : 19.340000/19.520000*0.224600/0.215000 })
 scaleFactor.update({'ggH_s'   : 19.340000/19.520000*0.224600/0.215000 })
