ec244abfdd01fe5b8533fe38b4256db756311571
amassiro <massironi.andrea@gmail.com>
2016-04-04 13:40:08 +0200

    new feature

---
+++ examples/doPruneNuisanceHWW.sh
@@ -0,0 +1,13 @@
+
+#
+# ggH 0/1 jet
+#
+
+# python PruneDatacard.py  \
+#         -d /afs/cern.ch/user/a/amassiro/Framework/CMSSW_7_6_3/src/PlotsConfigurations/Configurations/ggHTest/datacards/hww2l2v_13TeV_em_0j/mllVSmth/datacard.txt  \
+#         -o /afs/cern.ch/user/a/amassiro/Framework/CMSSW_7_6_3/src/PlotsConfigurations/Configurations/ggHTest/datacards/hww2l2v_13TeV_em_0j/mllVSmth/datacard.txt  \
+#         -i examples/input_nuisances_to_prune.py    
+        
+ls /afs/cern.ch/user/a/amassiro/Framework/CMSSW_7_6_3/src/PlotsConfigurations/Configurations/ggHTest/datacards/*/*/*.txt  | grep -v "pruned"  |   \
+    awk '{print "python PruneDatacard.py  -d "$1" -o "$1".pruned.txt    -i examples/input_nuisances_to_prune.py"}' | /bin/sh
+
