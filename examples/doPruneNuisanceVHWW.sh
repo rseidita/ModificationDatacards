432e243ce22af147f907fc5d2395ecd9a16a501a
amassiro <massironi.andrea@gmail.com>
2016-06-24 10:38:41 +0200

    update for ICHEP

---
+++ examples/doPruneNuisanceVHWW.sh
@@ -0,0 +1,9 @@
+
+#
+# VH 2 jet
+#
+
+
+ls /afs/cern.ch/user/a/amassiro/Framework/CMSSW_8_0_5/src/PlotsConfigurations/Configurations/VH2j/datacards/*/*/*.txt  | grep -v "pruned"  |   \
+    awk '{print "python PruneDatacard.py  -d "$1" -o "$1".pruned.txt    -i examples/input_nuisances_to_prune.py"}' | /bin/sh
+
