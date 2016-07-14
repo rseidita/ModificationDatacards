40d7479ad8706cb39feda51b9820b638223e2ea4
amassiro <massironi.andrea@gmail.com>
2016-07-14 14:24:29 +0200

    update for ICHEP

--- examples/doPruneNuisanceVBFHWW.sh
+++ examples/doPruneNuisanceVBFHWW.sh
@@ -7,3 +7,8 @@
 ls /afs/cern.ch/user/a/amassiro/Framework/CMSSW_8_0_5/src/PlotsConfigurations/Configurations/VBF/datacards/*/*/*.txt  | grep -v "pruned"  |   \
     awk '{print "python PruneDatacard.py  -d "$1" -o "$1".pruned.txt --suppressNegative=True   -i examples/input_nuisances_to_prune.py"}' | /bin/sh
 
+
+ls /afs/cern.ch/user/a/amassiro/Framework/CMSSW_8_0_5/src/PlotsConfigurations/Configurations/VBF/Moriond/datacards/*/*/*.txt  | grep -v "pruned"  |   \
+    awk '{print "python PruneDatacard.py  -d "$1" -o "$1".pruned.txt --suppressNegative=True   -i examples/input_nuisances_to_prune.py"}' | /bin/sh
+
+    
--- examples/doPruneNuisanceVHWW.sh
+++ examples/doPruneNuisanceVHWW.sh
@@ -7,3 +7,7 @@
 ls /afs/cern.ch/user/a/amassiro/Framework/CMSSW_8_0_5/src/PlotsConfigurations/Configurations/VH2j/datacards/*/*/*.txt  | grep -v "pruned"  |   \
     awk '{print "python PruneDatacard.py  -d "$1" -o "$1".pruned.txt --suppressNegative=True   -i examples/input_nuisances_to_prune.py"}' | /bin/sh
 
+
+ls /afs/cern.ch/user/a/amassiro/Framework/CMSSW_8_0_5/src/PlotsConfigurations/Configurations/VH2j/Moriond/datacards/*/*/*.txt  | grep -v "pruned"  |   \
+    awk '{print "python PruneDatacard.py  -d "$1" -o "$1".pruned.txt --suppressNegative=True   -i examples/input_nuisances_to_prune.py"}' | /bin/sh
+
