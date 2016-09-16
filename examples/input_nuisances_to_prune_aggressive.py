9fce3270e7e909df019d2b2cc665a1d37c2b4d7a
amassiro <massironi.andrea@gmail.com>
2016-09-16 15:43:33 +0200

    new features added

---
+++ examples/input_nuisances_to_prune_aggressive.py
@@ -0,0 +1,13 @@
+#
+# Nuisances to prune
+#
+# nuisancesToPrune.update({'HRname'  : 'string'})
+# 
+#
+# remove niusance that matches "string" in the name
+#
+
+nuisancesToPrune.update({'statMC'  : '*ibin_*_stat*'})
+
+nuisancesToPrune.update({'jetScale': '*CMS_scale_j*'})
+
