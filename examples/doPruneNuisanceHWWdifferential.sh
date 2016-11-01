573c9ec0969e1101b2075bf6bda0f28dfb1cbf56
amassiro <massironi.andrea@gmail.com>
2016-11-01 18:20:06 +0100

    tiny

--- examples/doPruneNuisanceHWWdifferential.sh
+++ examples/doPruneNuisanceHWWdifferential.sh
@@ -3,6 +3,14 @@
 # ggH 0/1 jet differential
 #
         
-ls /afs/cern.ch/user/a/amassiro/Framework/CMSSW_7_6_3/src/PlotsConfigurations/Configurations/Differential/ggH/datacards/*/*/*.txt  | grep -v "pruned"  |   \
+# ls /afs/cern.ch/user/a/amassiro/Framework/CMSSW_7_6_3/src/PlotsConfigurations/Configurations/Differential/ggH/datacards/*/*/*.txt  | grep -v "pruned"  |   \
+#     awk '{print "python PruneDatacard.py  -d "$1" -o "$1".pruned.txt    -i examples/input_nuisances_to_prune.py"}' | /bin/sh
+
+
+ls /afs/cern.ch/user/a/amassiro/Framework/CMSSW_8_0_5/src/PlotsConfigurations/Configurations/Differential/ggH/datacards/*/*/*.txt  | grep -v "pruned"  |   \
     awk '{print "python PruneDatacard.py  -d "$1" -o "$1".pruned.txt    -i examples/input_nuisances_to_prune.py"}' | /bin/sh
 
+
+ls /afs/cern.ch/user/a/amassiro/Framework/CMSSW_8_0_5/src/PlotsConfigurations/Configurations/Differential/ggH/pt1/datacards/*/*/*.txt  | grep -v "pruned"  |   \
+    awk '{print "python PruneDatacard.py  -d "$1" -o "$1".pruned.txt    -i examples/input_nuisances_to_prune.py"}' | /bin/sh
+    
