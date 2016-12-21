b90c371bfd0fb5a70a5038e09efa6780c1cf9335
amassiro <massironi.andrea@gmail.com>
2016-12-21 14:42:06 +0100

    fakes, don't prune them

--- examples/input_nuisances_to_prune.py
+++ examples/input_nuisances_to_prune.py
@@ -7,5 +7,21 @@
 # remove niusance that matches "string" in the name
 #
 
-nuisancesToPrune.update({'statMC'  : '*ibin_*_stat*'})
+#nuisancesToPrune.update({'statMC'  : '**ibin_*_stat*'})
+
+nuisancesToPrune.update({'statMCggZH'  : '*ggZH*ibin_*_stat*'})
+#nuisancesToPrune.update({'statMC'  : '*qqH*ibin_*_stat*'})
+nuisancesToPrune.update({'statMCWH'    : '*WH*ibin_*_stat*'})
+#nuisancesToPrune.update({'statMC'  : '*ggH*ibin_*_stat*'})
+nuisancesToPrune.update({'statMCHtt'   : '*H_htt*ibin_*_stat*'})
+nuisancesToPrune.update({'statMCZH'    : '*ZH*ibin_*_stat*'})
+nuisancesToPrune.update({'statMCggWW'  : '*ggWW*ibin_*_stat*'})
+nuisancesToPrune.update({'statMCVg'    : '*Vg*ibin_*_stat*'})
+nuisancesToPrune.update({'statMCVVV'   : '*VVV*ibin_*_stat*'})
+nuisancesToPrune.update({'statMCtop'   : '*top*ibin_*_stat*'})
+nuisancesToPrune.update({'statMCDY'    : '*DY*ibin_*_stat*'})
+nuisancesToPrune.update({'statMCWW'    : '*WW*ibin_*_stat*'})
+#nuisancesToPrune.update({'statMC'  : '*Fake*ibin_*_stat*'})
+nuisancesToPrune.update({'statMCVZ'    : '*VZ*ibin_*_stat*'})
+nuisancesToPrune.update({'statMCVgS'   : '*VgS*ibin_*_stat*'})
 
